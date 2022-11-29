416b85e07448f343d9edbe134e3adf4749928239
Lumierra <tripoernomoaji@gmail.com>
2022-11-29 10:03:42 +0700

    upload assets

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -15,18 +16,19 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var string[]
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
@@ -36,9 +38,94 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasAnyRoles($roles)
+    {
+        if ($this->roles()->whereIn('name', $roles)->first()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function hasRole($role)
+    {
+        if ($this->roles()->where('name', $role)->first()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    public function getRole($role)
+    {
+        return $this->role()->where('name', $role)->first();
+    }
+    public function getAnyRole($role)
+    {
+        return $this->role()->whereIn('name', $role)->first();
+    }
+
+    public function unit()
+    {
+        return $this->belongsTo(Unit::class);
+    }
+
+    // public function getCoba($hari)
+    // {
+    //     $date = Carbon::createFromDate(date('Y'), date('m'), $hari)->format('Y-m-d');
+    //     $data = Data::where('tanggal', $date)->get()->count();
+    //     // return $data;
+    //     return rand(5,15);
+    // }
+
+    public function isRole()
+    {
+        return $this->role();
+    }
+
+
+    public function submenus()
+    {
+        return $this->belongsToMany(Submenu::class);
+    }
+
+    public function menuuser()
+    {
+        return $this->hasMany(MenuUser::class);
+    }
+
+    public function getMenu()
+    {
+        return $this->menuuser()->where('user_id', $this->id)->orderBy('menu_id')->get();
+    }
+
+    public function getAllMenu()
+    {
+        return Menu::where('status', 'aktif')->orderBy('urut', 'asc')->get();
+    }
+
+    public function gender()
+    {
+        return $this->belongsTo(Gender::class);
+    }
 }
